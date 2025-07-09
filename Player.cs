2f2958c7996b7a14c295752503d7c42547c42297
Danik <mr.granddd@gmail.com>
2025-07-09 19:34:07 +0300

    Убрал классы в отдельный файл

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_1;
+
+public class Player
+{
+    public readonly string ClassName;
+    public readonly int Lvl;
+    public readonly string Name;
+
+    public Player(string name, int lvl, string className)
+    {
+        Name = name;
+        Lvl = lvl;
+        ClassName = className;
+    }
+
+    public Player()
+    {
+        Name = "Unknown";
+        Lvl = 0;
+        ClassName = "Unknown";
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"\nPlayer - {Name}\nLevel - {Lvl}\nClass - {ClassName}\n");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,35 +7,8 @@ internal class Program
         Player player1 = new("Danik", 24, "Archer");
         Player player2 = new("Khel", 40, "Warrior");
         Player player3 = new();
-
         player1.ShowInfo();
         player2.ShowInfo();
         player3.ShowInfo();
-    }
-
-    private class Player
-    {
-        public readonly string ClassName;
-        public readonly int Lvl;
-        public readonly string Name;
-
-        public Player(string name, int lvl, string className)
-        {
-            Name = name;
-            Lvl = lvl;
-            ClassName = className;
-        }
-
-        public Player()
-        {
-            Name = "Unknown";
-            Lvl = 0;
-            ClassName = "Unknown";
-        }
-
-        public void ShowInfo()
-        {
-            Console.WriteLine($"\nPlayer - {Name}\nLevel - {Lvl}\nClass - {ClassName}\n");
-        }
     }
 }
